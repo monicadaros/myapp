585b2e2bb162844d236be0036d1596e9f7985f29
Monica Daros <monica.daros@warren.com.br>
2022-05-17 11:10:06 -0300

    EV-221 - Testing Charts

--- lib/charts_flutter/chart_graphic.dart
+++ lib/charts_flutter/chart_graphic.dart
@@ -19,49 +19,47 @@ class _GraficoTesteState extends State<GraficoTeste> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Syncfusion Flutter chart'),
+    return Column(children: [
+      //Initialize the chart widget
+      SfCartesianChart(
+          primaryXAxis: CategoryAxis(isVisible: false),
+          backgroundColor: Color.fromARGB(255, 212, 212, 212),
+          // Chart title
+          title: ChartTitle(
+              text: 'R\$1000,00',
+              alignment: ChartAlignment.near,
+              textStyle: const TextStyle(height: 40, color: Colors.black)),
+          // Enable tooltip
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: <ChartSeries<_SalesData, String>>[
+            LineSeries<_SalesData, String>(
+                animationDuration: 1000,
+                dataSource: data,
+                xValueMapper: (_SalesData sales, _) => sales.year,
+                yValueMapper: (_SalesData sales, _) => sales.sales,
+                // Enable data label
+                dataLabelSettings: DataLabelSettings(isVisible: true))
+          ]),
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          //Initialize the spark charts widget
+          child: SfSparkLineChart.custom(
+            //Enable the trackball
+            trackball: SparkChartTrackball(
+                activationMode: SparkChartActivationMode.tap),
+            //Enable marker
+            marker:
+                SparkChartMarker(displayMode: SparkChartMarkerDisplayMode.all),
+            //Enable data label
+            labelDisplayMode: SparkChartLabelDisplayMode.all,
+            xValueMapper: (int index) => data[index].year,
+            yValueMapper: (int index) => data[index].sales,
+            dataCount: 5,
+          ),
         ),
-        body: Column(children: [
-          //Initialize the chart widget
-          SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              // Chart title
-              title: ChartTitle(text: 'Half yearly sales analysis'),
-              // Enable legend
-              legend: Legend(isVisible: true),
-              // Enable tooltip
-              tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries<_SalesData, String>>[
-                LineSeries<_SalesData, String>(
-                    dataSource: data,
-                    xValueMapper: (_SalesData sales, _) => sales.year,
-                    yValueMapper: (_SalesData sales, _) => sales.sales,
-                    name: 'Sales',
-                    // Enable data label
-                    dataLabelSettings: DataLabelSettings(isVisible: true))
-              ]),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              //Initialize the spark charts widget
-              child: SfSparkLineChart.custom(
-                //Enable the trackball
-                trackball: SparkChartTrackball(
-                    activationMode: SparkChartActivationMode.tap),
-                //Enable marker
-                marker: SparkChartMarker(
-                    displayMode: SparkChartMarkerDisplayMode.all),
-                //Enable data label
-                labelDisplayMode: SparkChartLabelDisplayMode.all,
-                xValueMapper: (int index) => data[index].year,
-                yValueMapper: (int index) => data[index].sales,
-                dataCount: 5,
-              ),
-            ),
-          )
-        ]));
+      )
+    ]);
   }
 }
 
